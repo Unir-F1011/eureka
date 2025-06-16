e654c8ef776cd765aca22f2640380ff204153a5c
MBI-88 <ingmbi8807@gmail.com>
2025-06-16 08:15:33 -0300

    layout done

---
+++ src/test/java/eureka/com/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package eureka.com.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
